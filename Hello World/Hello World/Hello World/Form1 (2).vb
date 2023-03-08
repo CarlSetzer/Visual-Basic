38b8ef0f12eba637927b2b53e03a079c32c7773f
Carl Setzer <carl@carlsetzer.com>
2023-03-07 17:40:55 -0800

    Uploaded my first VB project

---
+++ Hello World/Hello World/Hello World/Form1 (2).vb
@@ -0,0 +1,13 @@
+﻿/*Carl Setzer*/
+/*CS 115 - Professor Jean Liu*/
+/*Lab 1*/
+Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblMessage.Click
+
+    End Sub
+
+    Private Sub btnDisplayMessage_Click(sender As Object, e As EventArgs) Handles btnDisplayMessage.Click
+        lblMessage.Text = "Hello World"
+
+    End Sub
+End Class
